ca72cc3f9aed4529ecb42728716e26d9cc182ae5
afuentesIA <afuentes@lerobotics.ai>
2026-06-04 12:46:51 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
-import Navbar    from './components/Navbar'
-import Footer    from './components/Footer'
-import Home      from './pages/Home'
-import Products  from './pages/Products'
-import About     from './pages/About'
-import Mandate   from './pages/Mandate'
-import Logistics from './pages/Logistics'
-import Contact   from './pages/Contact'
+import { Helmet } from 'react-helmet-async'
+import Navbar     from './components/Navbar'
+import Footer     from './components/Footer'
+import Home       from './pages/Home'
+import Products   from './pages/Products'
+import About      from './pages/About'
+import Allocations from './pages/Allocations'
+import Logistics  from './pages/Logistics'
+import Contact    from './pages/Contact'
 import './index.css'
 
 function ScrollReset() {
@@ -20,14 +21,22 @@ function Layout() {
   return (
     <>
       <ScrollReset />
+      <Helmet>
+        <html lang="en" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Inter:wght@300;400;500;600&family=Archivo+Narrow:wght@400;500;600&display=swap" rel="stylesheet" />
+      </Helmet>
       <Navbar />
       <Routes>
-        <Route path="/"          element={<Home />}      />
-        <Route path="/products"  element={<Products />}  />
-        <Route path="/about"     element={<About />}     />
-        <Route path="/mandate"   element={<Mandate />}   />
-        <Route path="/logistics" element={<Logistics />} />
-        <Route path="/contact"   element={<Contact />}   />
+        <Route path="/"           element={<Home />}        />
+        <Route path="/products"   element={<Products />}    />
+        <Route path="/about"      element={<About />}       />
+        <Route path="/allocations" element={<Allocations />} />
+        <Route path="/logistics"  element={<Logistics />}   />
+        <Route path="/contact"    element={<Contact />}     />
       </Routes>
       <Footer />
     </>
